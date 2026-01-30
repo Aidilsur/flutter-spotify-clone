77facfc7bf0ce08751b9b88c4de642e5b522dbb1
saidil <aidilsurya9@gmail.com>
2026-01-30 17:16:11 +0700

    signin or signup page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone/core/config/theme/app_theme.dart';
-import 'package:spotify_clone/presentation/choose_mode/blox/theme_cubit.dart';
+import 'package:spotify_clone/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify_clone/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
---
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/helpers/is_dark_mode.dart';
+import 'package:spotify_clone/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone/core/config/assets/app_images.dart';
+import 'package:spotify_clone/core/config/assets/app_vectors.dart';
+import 'package:spotify_clone/core/config/theme/app_colors.dart';
+
+class SignupOrSigninPage extends StatelessWidget {
+  const SignupOrSigninPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          BasicAppbar(),
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.bottomPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(AppImages.authBG),
+          ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(AppVectors.logo),
+                SizedBox(height: 55),
+                const Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                SizedBox(height: 21),
+                const Text(
+                  'Spotify is a propietary Swedish audio streaming and media services provider',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 13,
+                    color: AppColors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicAppButton(
+                          onPressed: () {},
+                          title: 'Register',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: context.isDarkMode
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
